0b94c1ad7054534d596eb76ebb10eb17f7e934d7
DavidCastro21 <davidjscastro1001@gmail.com>
2025-07-07 00:08:14 +0100

    exercises 1,2,3,4

--- diet_estimation_skill_server/include/diet_estimation_skill_server/diet_estimation_skill_server.h
+++ diet_estimation_skill_server/include/diet_estimation_skill_server/diet_estimation_skill_server.h
@@ -1,3 +1,6 @@
+#ifndef DIET_ESTIMATION_SKILL_SERVER_H
+#define DIET_ESTIMATION_SKILL_SERVER_H
+
 // ROS includes
 #include <ros/ros.h>
 #include <ros/package.h>
@@ -12,20 +15,18 @@
 #include <diet_estimation_skill_server/diet_estimation_base.h>
 #include <diet_estimation_skill_server/heuristics/protein_selector.h>
 #include <diet_estimation_skill_server/heuristics/fiber_selector.h>
-
-
+#include <diet_estimation_skill_server/heuristics/fat_selector.h>
+#include <diet_estimation_skill_server/heuristics/caloric_selector.h>
 
 // Std includes
 #include <memory>
-#include <utility>      // std::pair
-#include <numeric>      // accumulate
-
+#include <utility>
+#include <numeric>
 
 namespace diet_estimation_skill {
     class DietEstimationSkill {
     public:
         typedef actionlib::SimpleActionServer<diet_estimation_skill_msgs::DietEstimationSkillAction> DietEstimationSkillActionServer;
-        typedef actionlib::SimpleActionServer<diet_estimation_skill_msgs::DietEstimationSkillAction> TriggerSkillActionServer;
 
         enum OPERATION_MODE {
             DIRECT,
@@ -33,72 +34,50 @@ namespace diet_estimation_skill {
             STANDALONE_RUN
         };
 
-        //Contructor
-        DietEstimationSkill ();
+        DietEstimationSkill();
 
-        ~DietEstimationSkill (void);
+        ~DietEstimationSkill();
 
+        void start();
 
-        void start (); //start the server
+        void executeCB(const diet_estimation_skill_msgs::DietEstimationSkillGoalConstPtr &goal);
 
-        void executeCB (
-                const diet_estimation_skill_msgs::DietEstimationSkillGoalConstPtr &goal); // recognize the goal request
+        void feedback(float percentage);
 
-        void feedback (float percentage);
+        void setSucceeded(std::string outcome = "succeeded");
 
-        void setSucceeded (std::string outcome = "succeeded");
+        void setAborted(std::string outcome = "aborted");
 
-        void setAborted (std::string outcome = "aborted");
+        bool checkPreemption();
 
-        bool checkPreemption ();
-
-        bool setupSkillConfigurationFromParameterServer (ros::NodeHandlePtr &_node_handle,
-                                                         ros::NodeHandlePtr &_private_node_handle);
-
-
-
+        bool setupSkillConfigurationFromParameterServer(ros::NodeHandlePtr &_node_handle,
+                                                       ros::NodeHandlePtr &_private_node_handle);
 
     protected:
-
         ros::NodeHandlePtr node_handle_;
         ros::NodeHandlePtr private_node_handle_;
-        ros::Publisher     pub_candidates_,
-                           pub_ws_cloud_;
-
-
         std::shared_ptr<DietEstimationSkillActionServer> actionServer_;
-        std::string                                       action_server_name_;
+        std::string action_server_name_;
 
-
-        diet_estimation_skill_msgs::DietEstimationSkillFeedback feedback_, trigger_feedback_;
-        diet_estimation_skill_msgs::DietEstimationSkillResult   result_, trigger_result_;
-
+        diet_estimation_skill_msgs::DietEstimationSkillFeedback feedback_;
+        diet_estimation_skill_msgs::DietEstimationSkillResult result_;
         diet_estimation_skill_msgs::Candidate msg_result_candidate_;
 
-        DietEstimationBase::ArrPtr estimationPipelineArrPtr_ = std::make_shared<DietEstimationBase::Arr>();
-
+        DietEstimationBase::ArrPtr estimationPipelineArrPtr_;
         DietEstimationBase::CandidateList candidate_list_;
-
-        bool preload_ok_;
-        int number_of_candidates_ = 0;
+        int number_of_candidates_;
         std::string patient_name_;
+        std::string preloaded_patient_name_;
 
-        bool loadEstimationPipeline ();
-        bool readDataCandidatesFromParameterServer() ;
-        bool executeProcess (int _operation_mode);
-        bool executeDirectProcess ();
-       // bool executePreLoad ();
-       // bool executeStandaloneProcess ();
+        bool loadEstimationPipeline();
+        bool readDataCandidatesFromParameterServer();
+        bool executeProcess(int _operation_mode);
+        bool executeDirectProcess();
         bool getNumberOfCandidates();
-        bool checkNamespace (std::string _input);
-        bool runMethods () ;
-
+        bool checkNamespace(std::string _input);
+        bool runMethods();
         bool getResult();
-
-
-
-
-
-
-        };
+    };
 }
+
+#endif
--- diet_estimation_skill_server/src/diet_estimation_skill_server.cpp
+++ diet_estimation_skill_server/src/diet_estimation_skill_server.cpp
@@ -109,8 +109,11 @@ namespace diet_estimation_skill {
         feedback (0);
         if (_operation_mode == OPERATION_MODE::DIRECT){ //Load and Run the Pipeline
             return (executeDirectProcess());
+        } if else (_operation_mode == OPERATION_MODE::PRE_LOAD){
+            return (preloadProcess()):
+        }if else (_operation_mode == OPERATION_MODE::STANDALONE_RUN){
+            return (standaloneProcess());
         }
-
         return true;
 
     }
@@ -118,7 +121,7 @@ namespace diet_estimation_skill {
     /// <summary>
     /// Execute the load and run mode together
     /// </summary>
-    /// <returns>true if succeeded, otherwise false </returns>
+    / // <returns>true if succeeded, otherwise false </returns>
     bool DietEstimationSkill::executeDirectProcess () {
 
         if (     !getNumberOfCandidates()
@@ -168,6 +171,10 @@ namespace diet_estimation_skill {
 
                 } else if (method_name == "fiber_scorer") {
                     method.reset(new FiberSelector());
+                } else if (method_name == "fat_scorer") {
+                    method.reset(new FatSelector());
+                } else if (method_name == "caloric_scorer") {
+                    method.reset(new CaloricSelector());
                 }
                 else{
                     ROS_ERROR_STREAM("Method name " << method_name << " is not supported by the server.");
@@ -261,6 +268,8 @@ namespace diet_estimation_skill {
             private_node_handle_->param(base_tree + "/gram", candidate.gram,1.0);
             private_node_handle_->param(base_tree + "/protein_rate", candidate.protein_rate,1.0);
             private_node_handle_->param(base_tree + "/fiber_rate", candidate.fiber_rate,1.0);
+            private_node_handle_->param(base_tree + "/fat_rate", candidate.fat_rate,1.0);
+
 
 
 
@@ -271,7 +280,8 @@ namespace diet_estimation_skill {
             ROS_DEBUG_STREAM(
                     current_candidate + " added: [Name: " << candidate.food_name <<
                                         " \n: Protein rate: " << candidate.protein_rate <<
-                                        " \n: Fiber Rate" << candidate.fiber_rate << "]");
+                                        " \n: Fiber Rate" << candidate.fiber_rate << 
+                                        " \n: Fat rate: " << candidate.fat_rate << "]");
         }
 
         feedback (30);
